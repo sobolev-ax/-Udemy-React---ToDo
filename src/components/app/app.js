9ecf32d61232817751b55e2c89e752e8d4701490
Aleksandr Sobolev <sobolev.ax@gmail.com>
2020-04-24 20:14:04 +0300

    Delete from state

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Header from '../header';
 import Search from '../search';
@@ -6,26 +6,37 @@ import List from '../list';
 
 import './app.css';
 
-const App = () => {
-  const todoData = [
-    { label: "Do sport", important: true, id: 1, },
-    { label: "Drink coffee", important: false, id: 2, },
-    { label: "Read book", important: false, id: 3, },
-  ];
+export default class App extends Component {
 
-  return (
-    <div className="container">
-      <div className="row justify-content-center">
-          <div className="col-xl-6">
+  state = {
+    todoData: [
+      { label: "Do sport", important: true, id: 1, },
+      { label: "Drink coffee", important: false, id: 2, },
+      { label: "Read book", important: false, id: 3, },
+    ],
+  }
 
-            <Header />
-            <Search />
-            <List data={ todoData } onDeleted={(id) => console.log('del', id)} />
+  deleteItem = (id) => {
+    this.setState(({ todoData }) => ({
+      todoData: todoData.filter((item) => item.id !== id)
+    }))
+  }
 
-          </div>
+  render() {
+    const { todoData } = this.state;
+
+    return (
+      <div className="container">
+        <div className="row justify-content-center">
+            <div className="col-xl-6">
+  
+              <Header />
+              <Search />
+              <List data={ todoData } onDeleted={ this.deleteItem } />
+  
+            </div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
-export default App;
