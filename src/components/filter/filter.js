145cfe18ea006c437b47ba3f78d766af5fc0f5e6
Aleksandr Sobolev <sobolev.ax@gmail.com>
2020-04-24 18:10:49 +0300

    Classes

--- src/components/filter/filter.js
+++ src/components/filter/filter.js
@@ -1,16 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './filter.css';
 
-const Filter = () => {
-  return (
-    <div className="btn-group">
-      <button type="button" className="btn btn-outline-info active">Left</button>
-      <button type="button" className="btn btn-outline-info">Middle</button>
-      <button type="button" className="btn btn-outline-info">Right</button>
-    </div>
-  );
+export default class Filter extends Component {
+  render() {
+    return (
+      <div className="btn-group">
+        <button type="button" className="btn btn-outline-info active">Left</button>
+        <button type="button" className="btn btn-outline-info">Middle</button>
+        <button type="button" className="btn btn-outline-info">Right</button>
+      </div>
+    );
+  }
 }
-
-export default Filter;
-
--- src/components/item/item.js
+++ src/components/item/item.js
@@ -1,27 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './item.css';
 
-const Item = ({ label, important = false }) => {
+export default class Item extends Component {
+  render() {
 
-  const isImportant = important ? 'text-danger' : '';
+    const { label, important = false } = this.props;
 
-  return (
-    <span className="d-flex justify-content-between">
-      <span className={`c-item-label ${isImportant}`}>
-        { label }
+    const isImportant = important ? 'text-danger' : '';
+
+    return (
+      <span className="d-flex justify-content-between">
+        <span className={`c-item-label ${isImportant}`}>
+          { label }
+        </span>
+
+        <span>
+          <button className="btn c-btn btn-sm btn-outline-danger mr-1" type="button">
+            Delete
+          </button>
+          <button className="btn c-btn btn-sm btn-outline-primary" type="button">
+            Toggle
+          </button>
+        </span>
       </span>
-
-      <span>
-        <button className="btn c-btn btn-sm btn-outline-danger mr-1" type="button">
-          Delete
-        </button>
-        <button className="btn c-btn btn-sm btn-outline-primary" type="button">
-          Toggle
-        </button>
-      </span>
-    </span>
-  );
+    );
+  }
 }
-
-export default Item;
