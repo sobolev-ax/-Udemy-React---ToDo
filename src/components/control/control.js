0170adb3128458b4dcc1f4bb47de1791bbab203f
Aleksandr Sobolev <sobolev.ax@gmail.com>
2020-04-25 18:21:40 +0300

    Forms

--- src/components/control/control.js
+++ src/components/control/control.js
@@ -1,15 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Control = ({ onAdded }) => {
-  return (
-    <div className="mt-3">
-      <button type="button"
-        className="btn btn-primary"
-        onClick={ () => onAdded('Go to shop') } >
-        Go to shop
-      </button>
-    </div>
-  );
+export default class Control extends Component {
+
+  state = {
+    label: '111'
+  }
+
+  change = (e) => {
+    this.setState({
+      label: e.target.value
+    })
+  }
+
+  submit = (e) => {
+    e.preventDefault();
+
+    const { onAdded } = this.props;
+
+    this.setState(({ label }) => {
+
+      if(label === '')
+        return;
+
+      onAdded(label);
+
+      return {
+        label: ''
+      }
+    })
+  }
+
+  render() {
+    const { label } = this.state;
+
+    return (
+      <form className="mt-3" onSubmit={ this.submit }>
+        <div className="input-group">
+          <input type="text"
+            placeholder="Type here to add item..."
+            className="form-control"
+            onChange={ this.change }
+            value={ label } />
+          <div className="input-group-append">
+            <button type="submit"
+              className="btn btn-primary">
+              Add item
+            </button>
+          </div>
+        </div>
+      </form>
+    );
+  }
 }
-
-export default Control;
